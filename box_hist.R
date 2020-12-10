6014ca82e30ebe451b66940065a570acd17f622e
TomekMatuszek <tom.mateuszek@gmail.com>
2020-12-10 18:58:58 +0100

    zmiana stylu i kolorów

--- box_hist.R
+++ box_hist.R
@@ -8,11 +8,16 @@ dane = filter(dane, geo != "FRY5", geo != "FRY3", geo != "FRY4",
                   geo != "PT2", geo != "PT20", geo != "PT3", geo != "PT30")
 
 ggplot(dane_regiony, aes(x = region, y = values, col = region)) +
-  coord_flip() + scale_color_manual(values = c("orange", "black", "darkgreen", "blue", "red")) +
+  coord_flip() + scale_color_manual(values = c("tomato", "black", "chartreuse4", "dodgerblue3", "violetred3")) +
   #geom_boxplot(col = "black") +
-  labs(y = "Wspolczynnik dzietnosci", x = "", col = "Region") +
-  geom_hline(yintercept = mean(dane$values), color = "gray30", size = 1) +
-  geom_jitter(size = 2, alpha = 0.2, width = 0.3) +
+  labs(y = "Wspolczynnik dzietnosci", x = element_blank(), col = "Region") +
+  theme(legend.position = "none", 
+        plot.background = element_rect(fill = "#252525"), 
+        panel.background = element_rect(fill = "#252525"),
+        axis.text = element_text(size = 12, color = "#eeeeee"),
+        axis.title.x = element_text(color = "#eeeeee")) +
+  geom_hline(yintercept = mean(dane$values), color = "darkred", size = 1.5) +
+  geom_jitter(size = 2, alpha = 0.5, width = 0.3) +
   stat_summary(fun = mean, geom = "point", size = 5)
   
   
--- mapa.R
+++ mapa.R
@@ -16,7 +16,7 @@ map2 = tm_shape(map_data, is.master = TRUE) +
               palette = "viridis",
               breaks = c(1, 1.2, 1.4, 1.6, 1.8, 2, Inf),
               legend.hist = TRUE) +
-  tm_layout(frame.lwd = 1, bg.color = "gray90", attr.color = "black",
+  tm_layout(frame.lwd = 1, bg.color = "gray25", attr.color = "black",
             legend.position = c(0.79, 0.3),
             #legend.position = c(0.01, 0.01),
             legend.frame = TRUE,
@@ -24,8 +24,10 @@ map2 = tm_shape(map_data, is.master = TRUE) +
             legend.title.size = 1.1,
             legend.hist.width = 0.2,
             legend.hist.height = 0.2,
-            legend.hist.bg.color = "gray60") +
-  tm_compass(type = "4star", position = c("left", "bottom"))
+            legend.hist.bg.color = "gray40",
+            legend.bg.color = "gray40") +
+  tm_compass(type = "rose", position = c("left", "bottom"),
+             color.light = "gray40", size = 4)
 map2
 ?tm_layout
 ?tm_polygons
